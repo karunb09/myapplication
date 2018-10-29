97016a3b8be628d11b91215a974afb6e6306f7b3
karun <11141770+karunb09@users.noreply.github.com>
2018-10-28 19:50:27 -0500

    updated test.

---
+++ test/M03test.js
@@ -0,0 +1,9 @@
+QUnit.test('Testing my age calculator', function (assert) {
+    assert.equal();
+    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","Success");
+    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","created");
+    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","generated");
+    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","success");
+   // assert.equal(checkBirthDay(2.5, 2.5, 2.5, 2.5), 10, "works with four positive real numbers");
+    //assert.equal(checkBirthDay(10, -10), 0, "works with a positive and a negative");
+});
