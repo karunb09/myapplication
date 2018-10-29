9ccd7a128dbda3b51f84f2bb1e7c4e7cb84fe9b2
karun <11141770+karunb09@users.noreply.github.com>
2018-10-29 15:08:32 -0500

    css update

--- public/js/assignment01.js
+++ public/js/assignment01.js
@@ -1,39 +1,40 @@
-   $( function(){ $("#calculate").click(function () {
+$(function () {
+    $("#calculate").click(function () {
         var mdate = $("#birth_date").val().toString();
         var yearThen = parseInt(mdate.substring(0, 4), 10);
         var monthThen = parseInt(mdate.substring(5, 7), 10);
         var dayThen = parseInt(mdate.substring(8, 10), 10);
-
+        //var today; //= new Date();
         var birthday = new Date(yearThen, monthThen - 1, dayThen);
         todaybday(today, birthday)
         checkBirthDay(birthday)
     })
 })
-    var today = new Date();
 
-    function todaybday(today, birthday) {
-        if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
-            alert("Happy B'day!!!");
-        }
-
+var today = new Date();
+function todaybday(today, birthday) {
+    if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
+        alert("Happy B'day!!!");
     }
-    function checkBirthDay(birthday) {
-        var differenceInMilisecond = today.valueOf() - birthday.valueOf();
 
-        var year_age = Math.floor(differenceInMilisecond / 31536000000);
-        var day_age = (Math.floor((differenceInMilisecond % 31536000000) / 86400000)) % 30;
-        var month_age = Math.floor(day_age / 30);
+}
+function checkBirthDay(birthday) {
+    var differenceInMilisecond = today.valueOf() - birthday.valueOf();
 
-        //day_age = day_age % 30;
+    var year_age = Math.floor(differenceInMilisecond / 31536000000);
+    var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
+    var month_age = Math.floor(day_age / 30);
 
-        if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
-            return $("#exact_age").text("Invalid birthday - Please try again!");
-        }
-        else {
-            if(document.getElementById('exact_age')){
-             return   $("#exact_age").html("You are <span id=\"age\">" + year_age + " years " + month_age + " months " + day_age + " days</span> old");
-            }
-            return "You are 21 years 9 months 17 days old"
-        }
+    day_age = day_age % 30;
+
+    if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
+        return $("#exact_age").text("Invalid birthday - Please try again!");
     }
+    else {
+        if (document.getElementById('exact_age')) {
+            return $("#exact_age").html("You are <span id=\"age\">" + year_age + " years " + month_age + " months " + day_age + " days</span> old");
+        }
+        //return "You are 21 years 9 months 17 days old"
+    }
+}
 
--- test/M03test.js
+++ test/M03test.js
@@ -1,9 +1,9 @@
 QUnit.test('Testing my age calculator', function (assert) {
     assert.equal();
     assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","Success");
-    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","created");
-    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","generated");
-    assert.equal(checkBirthDay(01/19/1997),"You are 21 years 9 months 17 days old","success");
+    assert.equal(checkBirthDay(11/20/1990),"You are 27 years 0 months 17 days old","created");
+    assert.equal(checkBirthDay(10/21/1996),"You are 22 years 1 months 17 days old","generated");
+    assert.equal(checkBirthDay(04/26/1991),"You are 26 years 7 months 17 days old","success");
    // assert.equal(checkBirthDay(2.5, 2.5, 2.5, 2.5), 10, "works with four positive real numbers");
     //assert.equal(checkBirthDay(10, -10), 0, "works with a positive and a negative");
 });
